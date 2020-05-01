ebdb07b6b228d7d9e1f792bb6a9adbc861018d5d
Chad <chad.lickey@carestream.com>
2020-05-01 13:05:37 -0600

    updated

--- Ghost Hunter - EMF/Assets/scripts/EMF.cs
+++ Ghost Hunter - EMF/Assets/scripts/EMF.cs
@@ -23,6 +23,13 @@ public class EMF : MonoBehaviour {
 	public Text RawVectorValueY;
 	public Text RawVectorValueZ;
 	public Text GeoLocationValue;
+	public Text emfIntensityText;
+	private float emfIntensity = 0;
+	private float emfIntensity_previous = 0;
+	double previousReadingTimestamp = 0;
+	int readingDisplayDelay = 10;//seconds to delay the display of the reading.  calms the display down for the user
+	public int EMF_MediumThreshold = 400; //the threshold for a medium emf hit
+	public int EMF_LargeThreshold = 600; //the threshold for a large emf hit
 	public int GhostDiscoveryThreshold = 15;//the threshold for changes in trueHeading readings before it's considered a hit
 	public int ConsecutiveHitsThreshold = 5;//number of hits before it is considered a "big" hit
 	public int ConsecutiveHitsTimeFrame = 5;//number of seconds between hits for hits to be "consecutive"
@@ -31,12 +38,14 @@ public class EMF : MonoBehaviour {
 	public bool VibrateHandset = true;//whether the handset will vibrate on a big hit
     LookAtTargetController targetController;
 
-    // Use this for initialization
-    void Start () {
+	System.DateTime epochStart = new System.DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
+
+	// Use this for initialization
+	void Start () {
 		Input.location.Start();
 		Input.compass.enabled = true;
         targetController = transform.root.GetComponentInChildren<LookAtTargetController>();
-    }
+	}
 	
 	// Update is called once per frame
 	void Update () {
@@ -45,18 +54,59 @@ public class EMF : MonoBehaviour {
 		rawVector = Input.compass.rawVector;
 		hitTimeStamp = Input.compass.timestamp;
 
-        if (TrueHeadingValue != null)
-        {
-            TrueHeadingValue.text = trueHeading.ToString("f1");
-            MagneticHeadingValue.text = magneticHeading.ToString("f1");
-            RawVectorValueX.text = rawVector.x.ToString("f1");
-            RawVectorValueY.text = rawVector.y.ToString("f1");
-            RawVectorValueZ.text = rawVector.z.ToString("f1");
+		//if (TrueHeadingValue != null)
+		//{
+		//    TrueHeadingValue.text = trueHeading.ToString("f1");
+		//    MagneticHeadingValue.text = magneticHeading.ToString("f1");
+		//    RawVectorValueX.text = rawVector.x.ToString("f1");
+		//    RawVectorValueY.text = rawVector.y.ToString("f1");
+		//    RawVectorValueZ.text = rawVector.z.ToString("f1");
 
-            targetController.RawVector = rawVector;
-        }
+		//    targetController.RawVector = rawVector;
+		//}
+
 		
-		checkForGhosts();
+		//hitTimeStamp = (System.DateTime.UtcNow - epochStart).TotalSeconds;
+
+		//if (previousReadingTimestamp == 0)
+		//	previousReadingTimestamp = hitTimeStamp;
+
+		//var tempTimeDifference = hitTimeStamp - previousReadingTimestamp;
+
+		//if (tempTimeDifference > readingDisplayDelay)
+		//{
+		//	float temp = Mathf.Abs((float)previousReadingTimestamp - (float)hitTimeStamp);
+
+		//	previousReadingTimestamp = hitTimeStamp;
+		//}
+
+		if (rawVector != null)//there are results from our sensors
+		{
+			emfIntensity = rawVector.magnitude;
+
+			if (emfIntensity != emfIntensity_previous && (hitTimeStamp - previousReadingTimestamp > readingDisplayDelay))
+			{
+				previousReadingTimestamp = hitTimeStamp;
+				emfIntensity_previous = emfIntensity;
+
+				emfIntensityText.text = emfIntensity.ToString("f0");
+
+				//emfIntensityText.text = tempTimeDifference.ToString("f0");
+				//emfIntensityText.text = hitTimeStamp.ToString("f0");
+
+				if (BackgroundImage != null)
+				{
+					if (emfIntensity > EMF_LargeThreshold)
+						BackgroundImage.color = Color.red;
+					else if (emfIntensity > EMF_MediumThreshold)
+						BackgroundImage.color = Color.yellow;
+					else
+						BackgroundImage.color = Color.blue;
+				}
+			}
+		}
+		
+		//checkForGhosts();
 	}
 
     public void OnGUI()
@@ -117,14 +167,14 @@ public class EMF : MonoBehaviour {
 					consecutiveHits++;
 				else
 				{
-					if(consecutiveHits >= ConsecutiveHitsThreshold)//this is a "big" hit
-						saveHit();
+					//if(consecutiveHits >= ConsecutiveHitsThreshold)//this is a "big" hit
+					//	saveHit();
 					
 					consecutiveHits = 0;
 				}
 			}
-			else//no field fluctuations detected so we will look for other signs
-				detectEntities();
+			//else//no field fluctuations detected so we will look for other signs
+			//	detectEntities();
 		}
 		else
 			firstRun = false;
